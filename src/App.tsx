8a043c6f85992a84596158d056cebd05b585c2b6
Danielnisir <20211275@nbsc.edu.ph>
2024-03-21 11:32:08 +0800

    added home

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,7 @@ import {
 } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
 import { calculatorOutline, ellipse, personOutline, speedometerOutline, square, triangle } from 'ionicons/icons';
+import Home from './pages/home';
 import Tab1 from './pages/Tab1';
 import Tab2 from './pages/Tab2';
 import Tab3 from './pages/Tab3';
@@ -41,6 +42,17 @@ const App: React.FC = () => (
     <IonReactRouter>
       <IonTabs>
         <IonRouterOutlet>
+
+           {/* Application default route */}
+           <Route exact path="/">
+            <Redirect to="/home" />
+          </Route>
+
+          {/* Home Router */}
+          <Route exact path="/home">
+            <Home />
+          </Route>
+          
           <Route exact path="/tab1">
             <Tab1 />
           </Route>
