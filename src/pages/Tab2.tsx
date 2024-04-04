4fa1462902c9c92de451a89362ac424a296be81e
Danielnisir <20211275@nbsc.edu.ph>
2024-04-04 13:43:03 +0800

    added clickcounter to the Tab2.tsx

--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -1,8 +1,15 @@
+import React, { useState } from 'react';
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab2.css';
 
 const Tab2: React.FC = () => {
+  const [count, setCount] = useState(0);
+
+  const incrementCounter = () => {
+    setCount(count + 1);
+  };
+
   return (
     <IonPage>
       <IonHeader>
@@ -16,7 +23,12 @@ const Tab2: React.FC = () => {
             <IonTitle size="large">Tab 2</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 2 page" />
+        <ExploreContainer name="" />
+        <div style={{ textAlign: 'center', marginTop: '20px' }}>
+          <h2>Click Counter</h2>
+          <div>Count: {count}</div>
+          <button onClick={incrementCounter}>Click Me</button>
+        </div>
       </IonContent>
     </IonPage>
   );
