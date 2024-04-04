89f87994114c7e9002268060bf869453e2214c42
Danielnisir <20211275@nbsc.edu.ph>
2024-04-04 14:02:41 +0800

    added calculator to the Tab3.tsx

--- src/pages/Tab3.tsx
+++ src/pages/Tab3.tsx
@@ -1,22 +1,60 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import React, { useState } from 'react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonInput, IonButton } from '@ionic/react';
 import './Tab3.css';
 
 const Tab3: React.FC = () => {
+  const [expression, setExpression] = useState('');
+
+  const buttonClick = (value: string) => {
+    setExpression(prevExpression => prevExpression + value);
+  };
+
+  const clearExpression = () => {
+    setExpression('');
+  };
+
+  const calculateResult = () => {
+    try {
+      const result = eval(expression);
+      setExpression(result.toString());
+    } catch (error) {
+      setExpression('Error');
+    }
+  };
+
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 3</IonTitle>
+          <IonTitle>Calculator</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Tab 3</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name="Tab 3 page" />
+        <IonInput value={expression} readonly={true} />
+        <div>
+          <IonButton onClick={() => buttonClick('1')}>1</IonButton>
+          <IonButton onClick={() => buttonClick('2')}>2</IonButton>
+          <IonButton onClick={() => buttonClick('3')}>3</IonButton>
+          <IonButton onClick={() => buttonClick('+')}>+</IonButton>
+        </div>
+        <div>
+          <IonButton onClick={() => buttonClick('4')}>4</IonButton>
+          <IonButton onClick={() => buttonClick('5')}>5</IonButton>
+          <IonButton onClick={() => buttonClick('6')}>6</IonButton>
+          <IonButton onClick={() => buttonClick('-')}>-</IonButton>
+        </div>
+        <div>
+          <IonButton onClick={() => buttonClick('7')}>7</IonButton>
+          <IonButton onClick={() => buttonClick('8')}>8</IonButton>
+          <IonButton onClick={() => buttonClick('9')}>9</IonButton>
+          <IonButton onClick={() => buttonClick('*')}>*</IonButton>
+        </div>
+        <div>
+          <IonButton onClick={() => buttonClick('0')}>0</IonButton>
+          <IonButton onClick={() => buttonClick('/')}>/</IonButton>
+          <IonButton onClick={calculateResult}>=</IonButton>
+          <IonButton onClick={clearExpression}>Clear</IonButton>
+        </div>
       </IonContent>
     </IonPage>
   );
