4b25cbd85e660631b4b57001d1e0ccfd44d6466a
Danielnisir <20211275@nbsc.edu.ph>
2024-05-10 09:00:46 +0800

    added changes

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,8 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    legacy()
-  ],
+  plugins: [react()],
+  base: "/ion-t-susana/"
   test: {
     globals: true,
     environment: 'jsdom',
